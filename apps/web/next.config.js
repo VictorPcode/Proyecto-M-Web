8ffd02aea492e24f3bbf4057a1c813fef6ef91be
vgpr <enoughcode.enterprise@hotmail.com>
2026-05-02 17:01:21 -0300

    fix web build remove next-transpile-modules

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
-const withTM = require("next-transpile-modules")(["@movi/ui"]);
-
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["@movi/ui"]
 };
 
-module.exports = withTM(nextConfig);
+module.exports = nextConfig;
