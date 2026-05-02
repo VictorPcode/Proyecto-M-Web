203c5468ad3d24d59a9303be8385181881bbd48d
vgpr <enoughcode.enterprise@hotmail.com>
2026-05-02 16:12:55 -0300

    fix workspace dependency for api

--- tree.js
+++ tree.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 function printTree(dir, prefix = '') {
-  const ignore = ['node_modules', 'objects', 'webpack'];
+  const ignore = ['node_modules', 'objects', 'webpack', 'migrations', 'dist', 'build'];
   const items = fs.readdirSync(dir).filter(item =>!ignore.includes(item)); // ignorar node_modules
   items.forEach((item, index) => {
     const isLast = index === items.length - 1;
