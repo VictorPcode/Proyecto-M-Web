c1b0fd83a7fc02bed42a305ac955826408a00536
vgpr <enoughcode.enterprise@hotmail.com>
2026-05-10 17:16:42 -0300

    api/src/index.ts port

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1282,7 +1282,7 @@ passengers.on("connection", (socket) => {
 // ---------------------
 // Server
 // ---------------------
-const PORT = Number(process.env.PORT) || 8080;
+const PORT = Number(process.env.PORT) || 4000;
 const HOST = "0.0.0.0";
 
 async function startServer() {
